4f30d3622a3618f87e55a8259d5e2858a827a4f9
Noppakorn Jiravaranun <noppakorn@noppakorn.com>
2021-08-25 02:20:31 +0700

    d62 && ds01_most

---
+++ grader/d62_q1a_vote.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    size_t n, k;
+    cin >> n >> k;
+    map<string, int> m;
+
+    while (n--)
+    {
+        string s;
+        cin >> s;
+        m[s]++;
+    }
+    vector<int> v;
+    for (auto &x : m)
+    {
+        v.push_back(x.second);
+    }
+    sort(v.begin(), v.end());
+
+    if (k > v.size())
+    {
+        cout << *(v.begin()) << "\n";
+    }
+    else
+    {
+        cout << *(v.begin() + v.size() - k) << "\n";
+    }
+}
---
+++ grader/ds01_most.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0);
+    int round;
+    cin >> round;
+    map<string,int> m;
+    int max = 0;
+    while(round--) {
+        string s;
+        cin >> s;
+        m[s]++;
+        if (m[s] > max) max = m[s];
+    }
+
+    string out;
+    for (auto &p : m) {
+        if (p.second == max) {
+            stringstream ss;
+            ss << p.second;
+            out = p.first + " " + ss.str();
+        }
+    }
+    cout << out << endl;
+}
