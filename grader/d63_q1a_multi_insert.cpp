8e8c67974aa633f171e3fbc4820dc653d46f9b0b
Noppakorn Jiravaranun <noppakorn@noppakorn.com>
2021-08-25 15:03:26 +0700

    more d63

---
+++ grader/d63_q1a_multi_insert.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void multiple_add(vector<int> &v, int value, int position, int count) {
+    // write your code here
+    while (count--) {
+        v.insert(v.begin() + position, value);
+        position++;
+    }
+
+}
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int n, value, position, count;
+    cin >> n >> value >> position >> count;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+    multiple_add(v, value, position, count);
+    cout << "After call multiple_add" << endl << "Size = " << v.size() << endl;
+    for (auto &x : v)
+        cout << x << " ";
+    cout << endl;
+}
