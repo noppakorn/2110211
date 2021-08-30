487cc1f2ae9514936f42b27a2eadf640aa097632
Noppakorn Jiravaranun <noppakorn@noppakorn.com>
2021-08-30 10:59:20 +0700

    d57_q1_postfix implementation

--- grader/d57_q1_postfix/student.h
+++ grader/d57_q1_postfix/student.h
@@ -1,12 +1,43 @@
 #ifndef __STUDENT_H__
 #define __STUDENT_H__
+#include <bits/stdc++.h>
 #include <vector>
 
 using namespace std;
 
-int eval_postfix(vector<pair<int,int> > v) {
-  //WRITE YOUR CODE HERE
-  //DON"T FORGET TO RETURN THE RESULT
+int calculate(int l, int r, char op) {
+    if (op == '+') {
+        return l + r;
+    } else if (op == '-') {
+        return l - r;
+    } else if (op == '*') {
+        return l * r;
+    } else {
+        return l / r;
+    }
+}
+
+int eval_postfix(vector<pair<int, int>> v) {
+    // WRITE YOUR CODE HERE
+    // DON"T FORGET TO RETURN THE RESULT
+    map<int, char> m = {{0, '+'}, {1, '-'}, {2, '*'}, {3, '/'}};
+    stack<int> s;
+    int l,r;
+    for (auto x : v) {
+        switch (x.first) {
+        case 0:
+            r = s.top();
+            s.pop();
+            l = s.top();
+            s.pop();
+            s.push(calculate(l, r, m[x.second]));
+            break;
+        case 1:
+            s.push(x.second);
+            break;
+        }
+    }
+    return s.top();
 }
 
 #endif
