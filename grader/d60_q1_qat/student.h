8dcd444df43137771bd54488125172c120040e87
Noppakorn Jiravaranun <noppakorn@noppakorn.com>
2021-09-22 14:33:56 +0700

    d60_q1_qat

---
+++ grader/d60_q1_qat/student.h
@@ -0,0 +1,9 @@
+#ifndef __STUDENT_H_
+#define __STUDENT_H_
+template <typename T>
+T CP::queue<T>::operator[](int idx) {
+    if (idx < 0)
+        idx += mSize;
+    return mData[(mFront + idx) % mCap];
+}
+#endif
