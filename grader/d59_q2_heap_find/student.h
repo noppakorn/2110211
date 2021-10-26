64787b776d56cd1d3c40ff04a4f74bd07f99f0db
Noppakorn Jiravaranun <noppakorn@noppakorn.com>
2021-10-27 01:03:18 +0700

    final heap

--- grader/d59_q2_heap_find/student.h
+++ grader/d59_q2_heap_find/student.h
@@ -1,25 +1,14 @@
-#ifndef __STUDENT_H_
-#define __STUDENT_H_
-
-#include <stdio.h>
-#include "priority_queue.h"
-#include <iostream>
-#include <algorithm>
-#include <math.h>
-
-template <typename T,typename Comp >
-bool CP::priority_queue<T,Comp>::find(T k) const {
-  return std::find(mData, mData+mSize, k) != mData+mSize;
+#include <bits/stdc++.h>
+template <class T, class C> bool CP::priority_queue<T, C>::find(T k) const {
+    return std::find(mData, mData + mSize, k) != mData + mSize;
 }
-
-template <typename T,typename Comp >
-int CP::priority_queue<T,Comp>::find_level(T k) const {
-  //your code here
-  int pos = -1;
-  for (size_t i = 0; i < mSize; ++i) {
-    if (*(mData+i) == k) pos = i;
-  }
-  return pos < 1 ? pos : log2(pos+1);
+template <class T, class C>
+int CP::priority_queue<T, C>::find_level(T k) const {
+    int i = mSize - 1;
+    for (; i >= 0; --i) {
+        if (mData[i] == k) {
+            break;
+        }
+    }
+    return i == 0 ? i : log2(i);
 }
-
-#endif
--- grader/d60_q2_heap_height/student.h
+++ grader/d60_q2_heap_height/student.h
@@ -1,3 +1,6 @@
-#include<math.h>
-template<typename T,typename Comp>
-int CP::priority_queue<T,Comp>::height() const {return !mSize ? -1 : log2(mSize);}
+#pragma GCC optimize("O3")
+#define endl "\n";
+#include <math.h>
+template <class T, class c> int CP::priority_queue<T, c>::height() const {
+    return mSize ? int(log2(mSize)) : -1;
+}
