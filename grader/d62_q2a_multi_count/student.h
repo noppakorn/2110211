ece8f8dd76694d983d9e3802e0eff1c905c2c65d
Noppakorn Jiravaranun <noppakorn@noppakorn.com>
2021-10-28 01:42:55 +0700

    d62_q2a_multi_count : best runtime

--- grader/d62_q2a_multi_count/student.h
+++ grader/d62_q2a_multi_count/student.h
@@ -1,21 +1,28 @@
 #pragma GCC optimize("O3")
 #define endl string("\n");
-#include <ext/pb_ds/assoc_container.hpp>
+#include <bits/stdc++.h>
+
+size_t fast_mod(const int &input, const int &ceil) {
+    return input >= ceil ? input % ceil : input;
+};
+
 template <class T>
 std::vector<std::pair<T, size_t>>
 CP::queue<T>::count_multi(std::vector<T> &k) const {
+    // Look mom, I'm hall of fame now!
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
-    __gnu_pbds::cc_hash_table<T, size_t> m;
-    for (auto &x : k)
+    std::unordered_map<T, size_t> m(k.size() * 2);
+    for (const auto &x : k)
         m[x];
     for (size_t i = 0; i < mSize; ++i) {
-        auto t = m.find(mData[(mFront + i) % mCap]);
+        auto t = m.find(mData[fast_mod(mFront + i, mCap)]);
         if (t != m.end())
             t->second++;
     }
     std::vector<std::pair<T, size_t>> v;
-    for (auto &x : k)
+    v.reserve(k.size());
+    for (const auto &x : k)
         v.push_back({x, m[x]});
     return v;
 }
