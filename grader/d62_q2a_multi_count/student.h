1a543f04780f50a60af5d735a954f6c7f46e15e7
Noppakorn Jiravaranun <noppakorn@noppakorn.com>
2021-10-28 14:37:28 +0700

    d62_q2a_multi_count: 152 ms

--- grader/d62_q2a_multi_count/student.h
+++ grader/d62_q2a_multi_count/student.h
@@ -1,28 +1,46 @@
 #pragma GCC optimize("O3")
 #define endl string("\n");
+// Hey Meen, Thanks for the code!
+#define fast_mod(x, y) (x >= y ? (x) % (y) : x)
 #include <bits/stdc++.h>
+#include <variant>
 
-size_t fast_mod(const int &input, const int &ceil) {
-    return input >= ceil ? input % ceil : input;
-};
+using std::size_t;
 
-template <class T>
+template <typename T>
 std::vector<std::pair<T, size_t>>
 CP::queue<T>::count_multi(std::vector<T> &k) const {
-    // Look mom, I'm hall of fame now!
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::unordered_map<T, size_t> m(k.size() * 2);
-    for (const auto &x : k)
-        m[x];
-    for (size_t i = 0; i < mSize; ++i) {
-        auto t = m.find(mData[fast_mod(mFront + i, mCap)]);
-        if (t != m.end())
-            t->second++;
+    // Hey Plum, Thanks for the code!
+    const size_t N = mSize + 3UL;
+    std::variant<T, bool> key[N];
+    for (size_t i = 0; i < N; i++)
+        key[i] = false;
+    size_t *value = new size_t[N]();
+    for (size_t i = 0; i < mSize; i++) {
+        size_t cur = std::hash<T>()(mData[fast_mod(mFront + i, mCap)]) % N;
+        const T &ref = mData[fast_mod(mFront + i, mCap)];
+        while (std::holds_alternative<T>(key[cur]) &&
+               std::get<T>(key[cur]) != ref) {
+            if (cur+1 == N)
+                cur = 1;
+            else
+                ++cur;
+        }
+        key[cur] = ref;
+        value[cur]++;
     }
-    std::vector<std::pair<T, size_t>> v;
-    v.reserve(k.size());
-    for (const auto &x : k)
-        v.push_back({x, m[x]});
-    return v;
+    std::vector<std::pair<T, size_t>> ret;
+    ret.reserve(k.size());
+    for (const T &ref : k) {
+        size_t cur = std::hash<T>()(ref) % N;
+        while (std::holds_alternative<T>(key[cur]) &&
+               std::get<T>(key[cur]) != ref) {
+            if (cur+1 == N)
+                cur = 1;
+            else
+                ++cur;
+        }
+        ret.emplace_back(ref, value[cur]);
+    }
+    return ret;
 }
