a8c8b6792a4c798fca3f96f14f95761adada2821
Noppakorn Jiravaranun <noppakorn@noppakorn.com>
2021-11-15 10:06:43 +0700

    d63_q4a_leaves_count

---
+++ grader/d63_q4a_leaves_count/student.h
@@ -0,0 +1,27 @@
+#ifndef __STUDENT_H_
+#define __STUDENT_H_
+
+#include <algorithm>
+#include <iostream>
+
+// you can modify and use this function
+template <typename KeyT,
+          typename MappedT,
+          typename CompareT>
+size_t CP::map_bst<KeyT,MappedT,CompareT>::leaves_count(node* n) {
+  if (n == nullptr) return 0;
+  if (n->left == nullptr && n->right == nullptr) return 1;
+  return leaves_count(n->left) + leaves_count(n->right);
+}
+
+
+template <typename KeyT,
+          typename MappedT,
+          typename CompareT>
+size_t CP::map_bst<KeyT,MappedT,CompareT>::leaves_count() {
+  return leaves_count(mRoot);
+  //write your code here
+}
+
+
+#endif
